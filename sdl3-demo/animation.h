774999113c3f7a636a1fb1d317d306e13d15b5bf
Jose Luna <josemanuelmluna@gmail.com>
2026-07-01 16:18:55 +0800

    feat: added animation and timer classes

---
+++ sdl3-demo/animation.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "timer.h"
+
+class Animation
+{
+    private:
+        Timer timer;
+        int frameCount;
+
+    public:
+        Animation() : timer(0), frameCount(0) {}
+        Animation(int frameCount, float length) : frameCount(frameCount), timer(length)
+        { }
+
+        float getLength() const { return timer.getLength(); }
+        void step (float deltaTime) { timer.step(deltaTime); }
+        int currentFrame() const
+        {
+            return static_cast<int>(timer.getTime() / timer.getLength() * frameCount);
+        }
+};
---
+++ sdl3-demo/timer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+class Timer
+{
+    public:
+        Timer(float length) : length(length), time(0), timeout(false) 
+        {};
+
+        void step(float deltaTime)
+        {
+            time += deltaTime;
+            if (time >= length)
+            {
+                time -= length;
+                timeout = true;
+            }
+        }
+
+        bool isTimeout() const { return timeout; }
+        float getTime() const { return time; }
+        float getLength() const { return length; }
+        void reset() { time = 0; }
+
+    private:
+        float length, time;
+        bool timeout;
+};
