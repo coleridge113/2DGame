ec587ea03a7f61a5f55c02d573a7a32abe2a5dae
Jose Luna <josemanuelmluna@gmail.com>
2026-07-01 13:52:54 +0800

    feat: created project run commands

--- projectrc.sh
+++ projectrc.sh
@@ -3,3 +3,7 @@
 alias pset="cmake --preset default"
 alias pbuild="cmake --build --preset default"
 alias prun="cmake --build --preset default --target run"
+
+function pall() {
+    pset && pbuild && prun
+}
