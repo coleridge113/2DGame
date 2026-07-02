0c4a479ce7e071d6ff7b7222e37f8e888957668c
Jose Luna <josemanuelmluna@gmail.com>
2026-07-02 20:07:10 +0800

    refactor: updated run commands

--- projectrc.sh
+++ projectrc.sh
@@ -4,10 +4,12 @@ alias pset="cmake --preset default"
 alias pbuild="cmake --build --preset default"
 alias prun="cmake --build --preset default --target run"
 
-function pall() {
-    pset && pbuild && prun
-}
-
 function pclean() {
+    echo "Cleaning up build..."
     rm -rf build/
 }
+
+function pall() {
+    pclean && pset && pbuild && prun
+}
+
